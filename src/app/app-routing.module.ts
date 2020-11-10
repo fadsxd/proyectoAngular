82b23141ad9bd854208e24f6988437fff7904c4b
fadsxd <mickyfads5@gmail.com>
2020-11-09 21:52:43 -0400

    proyecto en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PagesComponent } from './pages/pages.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const appRoutes: Routes = [
+  {path: '', 
+  component: PagesComponent,
+  children: [
+    {path:'dashboard', component: DashboardComponent},
+    {path:'progress', component: ProgressComponent},
+    {path:'grafica1', component: Grafica1Component},
+    {path:'', redirectTo: '/dashboard',pathMatch: 'full'}
+  ]
+  },
+  
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'**', component: NopagefoundComponent}
+
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true});
